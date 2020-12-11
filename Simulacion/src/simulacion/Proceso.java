7782fca6f79ccb4c27c2f3a35cb9d0e39ecc4bf3
miller barahona <millermiller01@hotmail.com>
2020-12-11 12:24:12 -0500

    vista peque

--- Simulacion/src/simulacion/Proceso.java
+++ Simulacion/src/simulacion/Proceso.java
@@ -1,14 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package simulacion;
 
-/**
- *
- * @author Usuario
- */
+
 public class Proceso {
+    
+    private String estado;
+    private int tamano;
+    private String nombre;
+    private int id;
+    private int NoHilos;
+    private String recursos;
+
+    public Proceso(String estado, int tamano, String nombre, int id, int NoHilos, String recursos) {
+        this.estado = estado;
+        this.tamano = tamano;
+        this.nombre = nombre;
+        this.id = id;
+        this.NoHilos = NoHilos;
+        this.recursos = recursos;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(int tamano) {
+        this.tamano = tamano;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNoHilos() {
+        return NoHilos;
+    }
+
+    public void setNoHilos(int NoHilos) {
+        this.NoHilos = NoHilos;
+    }
+
+    public String getRecursos() {
+        return recursos;
+    }
+
+    public void setRecursos(String recursos) {
+        this.recursos = recursos;
+    }
+    
+    
     
 }
